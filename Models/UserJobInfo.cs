a74d8e7a2701ae5f32c4f4c93effdda117048af2
Babalola Daniel <74297323+BabalolaBrainiac@users.noreply.github.com>
2023-05-17 20:26:33 +0100

    created user job info and user salary models

---
+++ Models/UserJobInfo.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotnetAPI.Models
+{
+    public partial class UserJobInfo
+    {
+        public int UserId {get; set;}
+
+        public string JobTitle {get; set;}
+
+        public string Department {get; set;}
+
+        public UserJobInfo()
+        {
+            if(JobTitle == null) 
+            {
+                JobTitle = "";
+            }
+
+             if(Department == null) 
+            {
+                Department = "";
+            }
+        }
+    }
+}
---
+++ Models/UserSalary.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotnetAPI.Models
+{
+    public partial class UserSalary
+    {
+
+        public decimal Salary {get; set;}
+
+        public decimal AvgSalary {get; set;}
+        
+    }
+}
